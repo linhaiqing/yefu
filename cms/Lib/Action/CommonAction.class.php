7fde8e6b3948485abb31e89df98cc54b4d1623b9
haiqing.in <linhaiqing520@gmail.com>
2019-07-02 15:22:46 +0800

    1

--- cms/Lib/Action/CommonAction.class.php
+++ cms/Lib/Action/CommonAction.class.php
@@ -111,8 +111,8 @@ class CommonAction extends Action
 		$this->common_url['group_category_all'] = $this->config['site_url'] . '/category/all/all';
 		// $this->static_path =  '/static/default/'  ;
 		// $this->static_public =  '/static/';
-		$this->static_path = '/static/default/' ;
-		$this->static_public =  '/static/';
+		$this->static_path = 'http://img.hiyefu.com'.'/static/default/' ;
+		$this->static_public =  'http://img.hiyefu.com'.'/static/';
 		
 		     
 		$this->assign('static_path', $this->static_path);
